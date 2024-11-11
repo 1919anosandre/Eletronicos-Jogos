0a01ed017941f92780311d374598b2a7a13aa03e
André Luiz <133181905+1919anosandre@users.noreply.github.com>
2024-11-11 19:10:41 -0300

    Update login.js

--- login.js
+++ login.js
@@ -187,4 +187,3 @@ const loginForm = document.getElementById('loginForm');
         window.location.href = 'http://127.0.0.1:5500/station-games/index.html'; // Altere para a URL desejada
     });
 
-
